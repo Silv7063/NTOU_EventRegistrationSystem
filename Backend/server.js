a859b7a03e74d175263d26b0e423da042ef0bde5
Silv7063 <ss639174@gmail.com>
2025-01-08 11:39:19 +0800

    update for render

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // 使用中間件
 app.use(cors({// 跨域資源共享（CORS）
-  origin: ['http://localhost:8080'],
+  origin: ['https://your-frontend.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 })); 
 app.use(express.json());  // 解析 JSON 請求體
